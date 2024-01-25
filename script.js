0a3d2627535f174f2e76974295c238ae453a6cee
masternonnolnw <masternonnolnw@gmail.com>
2024-01-25 09:17:58 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Code to run after the DOM has loaded
+  console.log("DOM is ready!");
+
+  // You can add more JavaScript code here for interactivity
+});
